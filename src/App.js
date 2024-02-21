1fa03c76ade2231a7d6e9fa8f0a895d8d4b21925
onnkek <onnkek@gmail.com>
2024-02-21 16:34:31 +0500

    create react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TRANSFER SCHEME TO REACT
+          TRANSFER SCHEME TO REACT!
         </p>
         <p>
           coming soon...
