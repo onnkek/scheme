03ff927838b717a994522df4571255131bb7e9f1
onnkek <onnkek@gmail.com>
2024-02-23 18:20:21 +0500

    test dnd

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -1,29 +1,51 @@
+import { useCallback, useContext, useRef, useState } from "react";
 import Circle from "../Shapes/Circle/Circle";
 import Line from "../Shapes/Line/Line";
 import Text from "../Shapes/Text/Text";
+import { SVGContext } from "../../context/SVGContext";
 
 function Node(props) {
-
-  let cpOffset = 50;
-  let cpRadius = 8;
-  let height = 30;
+  const cpOffset = 50;
+  const cpRadius = 8;
+  const height = 30;
+  
+  const SVGRef = useContext(SVGContext)
+  const [coord, setCoord] = useState(props.point);
+  const lastCursor = useRef({ x: 0, y: 0 });
+  const coordRef = useRef(props.point);
+  
   let { point, width, number } = props;
 
   let cp = [];
   for (let i = 0; i < width / cpOffset - 1; i++) {
-    cp.push({ "x": point.x - width / 2 + (i + 1) * cpOffset, "y": point.y });
+    cp.push({ x: coord.x - width / 2 + (i + 1) * cpOffset, y: coord.y });
   }
 
+
   let nodeMD = (e) => {
-    console.log(e.clientX);
+    lastCursor.current = { x: e.clientX, y: e.clientY }
+    coordRef.current = coord;
+    SVGRef.current.addEventListener('mousemove', handleSVGMouseMove)
+  }
+
+  const handleSVGMouseMove = useCallback(e => {
+    setCoord({
+      x: coordRef.current.x + e.clientX - lastCursor.current.x,
+      y: coordRef.current.y + e.clientY - lastCursor.current.y,
+    });
+  }, [])
+
+  let nodeMU = (e) => {
+    lastCursor.current = { x: 0, y: 0 }
+    SVGRef.current.removeEventListener('mousemove', handleSVGMouseMove)
   }
 
   return (
     <>
-      <Line p1={{ "x": point.x - width / 2, "y": point.y }} p2={{ "x": point.x + width / 2, "y": point.y }} stroke="darkred" 
-            strokeWidth={height} onMD={nodeMD}/>
+      <Line p1={{ x: coord.x - width / 2, y: coord.y }} p2={{ x: coord.x + width / 2, y: coord.y }} stroke="darkred"
+        strokeWidth={height} onMD={nodeMD} onMU={nodeMU} />
       {cp.map(p => <Circle center={p} radius={cpRadius} fill="white" />)}
-      <Text point={{ "x": point.x - width / 2 - 20, "y": point.y + 7 }} fill="white" fontSize={20}>{number}</Text>
+      <Text point={{ x: coord.x - width / 2 - 20, y: coord.y + 7 }} fill="white" fontSize={20}>{number}</Text>
     </>
   );
 }
--- src/components/SVGPanel/SVGPanel.js
+++ src/components/SVGPanel/SVGPanel.js
@@ -1,9 +1,509 @@
+import { useContext, useRef } from 'react';
 import './SVGPanel.css';
+import { SelectContext } from '../../context/selectContext';
+import Node from '../Node/Node';
+import Branch from '../Branch/Branch';
+import { SVGContext } from '../../context/SVGContext';
+
+
 
 function SVGPanel(props) {
+  const scheme = {
+    "nodes": [
+      {
+        "name": "1",
+        "number": 1,
+        "coordinates":
+        {
+          "x": 350,
+          "y": 110
+        }
+        ,
+        "image": {
+          "line": {},
+          "text": {},
+          "cp": [
+            {
+              "coordinates": {
+                "x": 250,
+                "y": 110
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 300,
+                "y": 110
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 350,
+                "y": 110
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 400,
+                "y": 110
+              },
+              "circle": {},
+              "animateIn": 1,
+              "animateOut": 0
+            },
+            {
+              "coordinates": {
+                "x": 450,
+                "y": 110
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            }
+          ],
+          "animateIn": 0,
+          "animateOut": 0
+        },
+        "animateOut": 1,
+        "animateIn": 0
+      },
+      {
+        "name": "2",
+        "number": 2,
+        "coordinates":
+        {
+          "x": 400,
+          "y": 660
+        }
+        ,
+        "image": {
+          "line": {},
+          "text": {},
+          "cp": [
+            {
+              "coordinates": {
+                "x": 300,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 350,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 400,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 450,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 1,
+              "animateOut": 0
+            },
+            {
+              "coordinates": {
+                "x": 500,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 1,
+              "animateOut": 0
+            }
+          ],
+          "animateIn": 0,
+          "animateOut": 0
+        },
+        "animateIn": 0,
+        "animateOut": 1
+      },
+      {
+        "name": "3",
+        "number": 3,
+        "coordinates":
+        {
+          "x": 710,
+          "y": 210
+        }
+        ,
+        "image": {
+          "line": {},
+          "text": {},
+          "cp": [
+            {
+              "coordinates": {
+                "x": 610,
+                "y": 210
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 660,
+                "y": 210
+              },
+              "circle": {},
+              "animateIn": 1,
+              "animateOut": 0
+            },
+            {
+              "coordinates": {
+                "x": 710,
+                "y": 210
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 760,
+                "y": 210
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 810,
+                "y": 210
+              },
+              "circle": {},
+              "animateIn": 1,
+              "animateOut": 0
+            }
+          ],
+          "animateIn": 0,
+          "animateOut": 0
+        },
+        "animateIn": 0,
+        "animateOut": 1
+      },
+      {
+        "name": "4",
+        "number": 4,
+        "coordinates":
+        {
+          "x": 860,
+          "y": 660
+        }
+        ,
+        "image": {
+          "line": {},
+          "text": {},
+          "cp": [
+            {
+              "coordinates": {
+                "x": 760,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 1,
+              "animateOut": 0
+            },
+            {
+              "coordinates": {
+                "x": 810,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 1,
+              "animateOut": 0
+            },
+            {
+              "coordinates": {
+                "x": 860,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 910,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            },
+            {
+              "coordinates": {
+                "x": 960,
+                "y": 660
+              },
+              "circle": {},
+              "animateIn": 0,
+              "animateOut": 1
+            }
+          ],
+          "animateIn": 0,
+          "animateOut": 0
+        },
+        "animateIn": 0,
+        "animateOut": 1
+      }
+    ],
+    "branches": [
+      {
+        "name": "12",
+        "number1": 1,
+        "number2": 2,
+        "pole1": [],
+        "pole2": [],
+        "image": {
+          "line": {},
+          "list": [
+            {
+              "coordinates": {
+                "x": 400,
+                "y": 110
+              }
+            },
+            {
+              "coordinates": {
+                "x": 400,
+                "y": 660
+              }
+            }
+          ],
+          "pole1": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ],
+          "pole2": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ]
+        }
+      },
+      {
+        "name": "23",
+        "number1": 2,
+        "number2": 3,
+        "pole1": [],
+        "pole2": [],
+        "image": {
+          "line": {},
+          "list": [
+            {
+              "coordinates": {
+                "x": 450,
+                "y": 660
+              }
+            },
+            {
+              "coordinates": {
+                "x": 450,
+                "y": 380
+              }
+            },
+            {
+              "coordinates": {
+                "x": 660,
+                "y": 380
+              }
+            },
+            {
+              "coordinates": {
+                "x": 660,
+                "y": 210
+              }
+            }
+          ],
+          "pole1": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ],
+          "pole2": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ]
+        }
+      },
+      {
+        "name": "34",
+        "number1": 3,
+        "number2": 4,
+        "pole1": [],
+        "pole2": [],
+        "image": {
+          "line": {},
+          "list": [
+            {
+              "coordinates": {
+                "x": 810,
+                "y": 210
+              }
+            },
+            {
+              "coordinates": {
+                "x": 810,
+                "y": 660
+              }
+            }
+          ],
+          "pole1": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ],
+          "pole2": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ]
+        }
+      },
+      {
+        "name": "41",
+        "number1": 4,
+        "number2": 2,
+        "pole1": [],
+        "pole2": [],
+        "image": {
+          "line": {},
+          "list": [
+            {
+              "coordinates": {
+                "x": 760,
+                "y": 660
+              }
+            },
+            {
+              "coordinates": {
+                "x": 760,
+                "y": 580
+              }
+            },
+            {
+              "coordinates": {
+                "x": 500,
+                "y": 580
+              }
+            },
+            {
+              "coordinates": {
+                "x": 500,
+                "y": 660
+              }
+            }
+          ],
+          "pole1": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ],
+          "pole2": [
+            {
+              "coordinates": [
+                {
+                  "x": "",
+                  "y": ""
+                }
+              ],
+              "circle": "",
+              "nodePole": ""
+            }
+          ]
+        }
+      }
+    ]
+  };
+
+  const SVGRef = useRef();
+
+  const onMD = (e) => {
+    console.log(e.target)
+  }
+
   return (
-    <svg id='svg'>
-      {props.children}
+    <svg ref={SVGRef} id='svg' onMouseDown={onMD}>
+      <SVGContext.Provider value={SVGRef}>
+        {scheme.nodes.map((node) => <Node point={{ "x": node.coordinates.x, "y": node.coordinates.y }} width="300" number={node.number} />)}
+        {scheme.branches.map((branch) => <Branch points={branch.image.list} />)}
+      </SVGContext.Provider>
+
     </svg>
   );
 }
